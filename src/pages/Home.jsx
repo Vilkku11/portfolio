bbd60bbcdd65e4c43e3b079dfef5a3f5c80d246b
William Reima <william.reima@gmail.com>
2024-01-31 14:47:17 +0200

    page init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
 const Home = () => {
-  return <h1>is this home??</h1>;
+  return <h1 className="mt-20">is this home??</h1>;
 };
 export default Home;
---
+++ src/pages/W/Portfolio.jsx
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+  return <h1 className="mt-20">W</h1>;
+};
+
+export default Portfolio;
