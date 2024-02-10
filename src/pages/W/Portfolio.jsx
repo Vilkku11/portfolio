6fcd8b7d8e4c9a65c8cacfbd4ff4f3827ee4ab29
William Reima <william.reima@gmail.com>
2024-02-11 01:08:50 +0200

    conditional navbar, W page start, cursor component, theming

--- src/pages/W/Portfolio.jsx
+++ src/pages/W/Portfolio.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+import Cursor from "./components/Cursor";
 const Portfolio = () => {
-  return <h1 className="mt-20">W</h1>;
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const textEnter = () => {
+    setCursorVariant("text");
+  };
+
+  const textLeave = () => {
+    setCursorVariant("default");
+  };
+  return (
+    <div className="container relative mx-auto">
+      <div className="fixed inset-0 bg-black opacity-100">
+        <h1
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
+          className="mt-80 text-white text-9xl font-mono"
+        >
+          Welcome!
+        </h1>
+      </div>
+      <Cursor cursorVariant={cursorVariant} />
+    </div>
+  );
 };
 
 export default Portfolio;
