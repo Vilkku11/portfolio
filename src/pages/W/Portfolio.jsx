d4e3eb9986a98d452b79168ef44a78945294af94
William Reima <william.reima@gmail.com>
2024-02-14 00:29:41 +0200

    item aligment test

--- src/pages/W/Portfolio.jsx
+++ src/pages/W/Portfolio.jsx
@@ -12,15 +12,23 @@ const Portfolio = () => {
     setCursorVariant("default");
   };
   return (
-    <div className="min-h-screen bg-black">
-      <div className="flex-col text-center">
+    <div className="top-0 left-0 fixed w-full min-h-screen bg-black">
+      <div className="flex flex-col items-center">
         <h1
           onMouseEnter={textEnter}
           onMouseLeave={textLeave}
-          className="text-center inline-block mt-80 text-white text-9xl font-mono "
+          className=" max-w-max mt-80 text-white text-9xl font-mono "
         >
           Welcome!
         </h1>
+        <p
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
+          className="max-w-max mt-20 text-white font-mono"
+        >
+          text here and a bit more moremo remo ore more moremo remo ore more
+          moremo remo ore more moremo remo ore
+        </p>
       </div>
       <Cursor cursorVariant={cursorVariant} />
     </div>
