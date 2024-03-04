8c3f83f6595b7cc2fe0792be5b65ce25deb73597
William Reima <william.reima@gmail.com>
2024-03-05 00:06:54 +0200

    scrollbar start theme, footer start, project showcase idea

--- src/pages/W/Portfolio.jsx
+++ src/pages/W/Portfolio.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
 import Cursor from "./components/Cursor";
+import Footer from "./components/Footer";
+import ProjectShowCase from "./components/ProjectShowCase";
+
+import "./Portfolio.css";
+
 const Portfolio = () => {
   const [cursorVariant, setCursorVariant] = useState("default");
 
@@ -12,7 +17,7 @@ const Portfolio = () => {
     setCursorVariant("default");
   };
   return (
-    <div className="top-0 left-0 fixed w-full min-h-screen bg-black">
+    <div className="top-0 left-0 w-full bg-black">
       <div className="flex flex-col items-center">
         <h1
           onMouseEnter={textEnter}
@@ -26,10 +31,20 @@ const Portfolio = () => {
           onMouseLeave={textLeave}
           className="max-w-max mt-20 text-white font-mono"
         >
-          text here and a bit more moremo remo ore more moremo remo ore more
-          moremo remo ore more moremo remo ore
+          A small amount of text, maybe some introduction text or something like
+          that. Maybe a bit what kind of things could be added here on this page
+          or something like that.
         </p>
+        <h1
+          className="max-w-max mt-80 text-white text-6xl font-mono"
+          onMouseEnter={textEnter}
+          onMouseLeave={textLeave}
+        >
+          My projects
+        </h1>
+        <ProjectShowCase />
       </div>
+      <Footer />
       <Cursor cursorVariant={cursorVariant} />
     </div>
   );
