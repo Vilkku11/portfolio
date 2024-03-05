fafa5f26d785e22bb57c85df410fe58d3d054e20
William Reima <william.reima@gmail.com>
2024-03-06 01:06:00 +0200

    curve component, currently broken

---
+++ src/pages/W/components/curve/Curve.jsx
@@ -0,0 +1,74 @@
+import { useRef, useEffect } from "react";
+import "./Curve.css";
+const Curve = () => {
+  const path = useRef(null);
+  let progress = 0;
+  let time = Math.PI / 2;
+  let reqId = null;
+
+  useEffect(() => {
+    setPath(progress);
+  }, []);
+  const setPath = (progress) => {
+    const { innerWidth } = window;
+    path.current.setAttributeNS(
+      "",
+      "d",
+      `M0 50 Q${innerWidth / 2} ${50 + progress}, ${innerWidth} 50`
+    );
+  };
+
+  const handleMouseEnter = () => {
+    if (reqId) {
+      window.cancelAnimationFrame(reqId);
+      resetAnimation();
+    }
+  };
+
+  const handleMouse = (event) => {
+    const { movementY } = event;
+    progress += movementY;
+    setPath(progress);
+  };
+
+  const handleMouseLeave = () => {
+    animateOut();
+  };
+
+  const lerp = (x, y, a) => {
+    x * (1 - a) + y * a;
+  };
+
+  const animateOut = () => {
+    const newProgress = progress * Math.sin(time);
+    time += 0.2;
+    setPath(newProgress);
+    //progress = lerp(progress, 0, 0.025);
+
+    if (Math.abs(progress) > 0.75) {
+      reqId = window.requestAnimationFrame(animateOut);
+    } else {
+      resetAnimation();
+    }
+  };
+
+  const resetAnimation = () => {
+    time = Math.PI / 2;
+    progress = 0;
+  };
+
+  return (
+    <div className="line">
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouse}
+        onMouseLeave={handleMouseLeave}
+        className="box"
+      ></div>
+      <svg>
+        <path ref={path}></path>
+      </svg>
+    </div>
+  );
+};
+export default Curve;
