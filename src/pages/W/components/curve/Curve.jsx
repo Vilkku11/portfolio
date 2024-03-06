0e7ca0d608be705d0e1a73e9574040acbe8fe268
William Reima <william.reima@gmail.com>
2024-03-06 22:25:06 +0200

    Curve component fixed and functional

--- src/pages/W/components/curve/Curve.jsx
+++ src/pages/W/components/curve/Curve.jsx
@@ -5,16 +5,18 @@ const Curve = () => {
   let progress = 0;
   let time = Math.PI / 2;
   let reqId = null;
+  let x = 0.5;
 
   useEffect(() => {
     setPath(progress);
   }, []);
+
   const setPath = (progress) => {
     const { innerWidth } = window;
     path.current.setAttributeNS(
       "",
       "d",
-      `M0 50 Q${innerWidth / 2} ${50 + progress}, ${innerWidth} 50`
+      `M0 50 Q${innerWidth * x} ${50 + progress}, ${innerWidth} 50`
     );
   };
 
@@ -26,7 +28,9 @@ const Curve = () => {
   };
 
   const handleMouse = (event) => {
-    const { movementY } = event;
+    const { movementY, clientX } = event;
+    const { left, width } = path.current.getBoundingClientRect();
+    x = (clientX - left) / width;
     progress += movementY;
     setPath(progress);
   };
@@ -36,14 +40,14 @@ const Curve = () => {
   };
 
   const lerp = (x, y, a) => {
-    x * (1 - a) + y * a;
+    return x * (1 - a) + y * a;
   };
 
   const animateOut = () => {
     const newProgress = progress * Math.sin(time);
     time += 0.2;
     setPath(newProgress);
-    //progress = lerp(progress, 0, 0.025);
+    progress = lerp(progress, 0, 0.025);
 
     if (Math.abs(progress) > 0.75) {
       reqId = window.requestAnimationFrame(animateOut);
