9c57462493250f02f9cba7a719c631299aaab9d9
William Reima <william.reima@gmail.com>
2024-05-31 00:38:36 +0300

    dep update, start of portfolio

---
+++ src/pages/W/Portfolio.tsx
@@ -0,0 +1,15 @@
+import { useState, useRef } from "react";
+
+import "./Portfolio.css";
+
+const Portfolio = () => {
+  return (
+    <div className="background">
+      <div className="item-container">
+        <h1>Welcome!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
