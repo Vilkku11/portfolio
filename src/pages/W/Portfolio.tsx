06f469578017530d79891dcd8ad0d7a34389a03a
William Reima <william.reima@gmail.com>
2024-06-02 23:13:09 +0300

    start of Header component

--- src/pages/W/Portfolio.tsx
+++ src/pages/W/Portfolio.tsx
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 import Curve from "./components/curve/Curve";
 import Cursor from "./components/cursor/Cursor";
+import Header from "./components/header/Header";
 
 import "./Portfolio.css";
 
@@ -9,12 +10,15 @@ const Portfolio = () => {
   const stickyElement = useRef<HTMLHeadingElement | null>(null);
 
   return (
-    <div className="background">
-      <div className="item-container">
-        <h1 ref={stickyElement}>Welcome!</h1>
+    <div>
+      <Header />
+      <div className="background">
+        <div className="item-container">
+          <h1 ref={stickyElement}>Welcome!</h1>
+        </div>
+        <Curve />
+        <Cursor stickyElement={stickyElement} />
       </div>
-      <Curve />
-      <Cursor stickyElement={stickyElement} />
     </div>
   );
 };
