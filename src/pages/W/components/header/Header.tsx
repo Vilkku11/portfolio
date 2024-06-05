f9ef8c81d93dd9ddcd737e48aa40cc90feb16368
William Reima <william.reima@gmail.com>
2024-06-05 22:59:19 +0300

    header component and its initial styling

--- src/pages/W/components/header/Header.tsx
+++ src/pages/W/components/header/Header.tsx
@@ -1,9 +1,29 @@
 import "./Header.css";
 
+type HeaderLink = {
+  name: string;
+  href: string;
+};
+
 const Header = () => {
+  const HeaderLinks: HeaderLink[] = [
+    { name: "Start", href: "#init" },
+    { name: "Overview", href: "#sdf" },
+  ];
+
   return (
     <header className="header">
-      <div className="list"></div>
+      <nav className="nav">
+        <ul className="nav-list">
+          {HeaderLinks.map((link) => (
+            <li key={link.name}>
+              <a className="nav-item" href={link.href}>
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
