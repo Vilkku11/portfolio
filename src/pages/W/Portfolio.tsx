f07e618714f8504a29d0a95a2d488b5f9ea59bd4
William Reima <william.reima@gmail.com>
2024-06-07 16:22:17 +0300

    SlotText component

--- src/pages/W/Portfolio.tsx
+++ src/pages/W/Portfolio.tsx
@@ -5,6 +5,7 @@ import Cursor from "./components/cursor/Cursor";
 import Header from "./components/header/Header";
 
 import ParagraphWords from "./components/paragraph/ParagraphWords";
+import SlotText from "./components/SlotText";
 
 import "./Portfolio.css";
 
@@ -17,6 +18,9 @@ const Portfolio = () => {
       <div className="item-container">
         <h1 ref={stickyElement}>Welcome!</h1>
         <ParagraphWords text="test 12309 12430 91290 129 00492" />
+        <div className="slot-text">
+          this is a <SlotText />. to see how this is functioning
+        </div>
       </div>
       <Curve />
       <Cursor stickyElement={stickyElement} />
