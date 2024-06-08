f8c70720e7271a055d77439dcf72466f2f22fa6a
William Reima <william.reima@gmail.com>
2024-06-08 14:56:46 +0300

    fixed bug where animation would break after switching to another tab

--- src/pages/W/components/SlotText.tsx
+++ src/pages/W/components/SlotText.tsx
@@ -4,13 +4,30 @@ import { motion, AnimatePresence } from "framer-motion";
 const SlotText = () => {
   const words: string[] = ["test1", "test2", "test3", "test4"];
   const [currentWord, setCurrentWord] = useState<number>(0);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
 
   useEffect(() => {
+    const handleVisibility = () => {
+      if (document.hidden) {
+        setIsVisible(false);
+        return;
+      }
+      setIsVisible(true);
+    };
+
+    document.addEventListener("visibilitychange", handleVisibility);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibility);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isVisible) return;
+
     const interval = setInterval(() => {
       setCurrentWord((prevIndex) => {
-        console.log(prevIndex);
         if (prevIndex == words.length - 1) {
-          console.log("same length as " + words.length);
           return 0;
         }
         return prevIndex + 1;
@@ -18,7 +35,7 @@ const SlotText = () => {
     }, 2000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [isVisible]);
 
   return (
     <div>
