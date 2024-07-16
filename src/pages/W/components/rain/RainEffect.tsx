09136d04b29c9f99329c4f9d974423ce9ea46ba0
William Reima <william.reima@gmail.com>
2024-07-16 23:32:28 +0300

    initial rain effect

---
+++ src/pages/W/components/rain/RainEffect.tsx
@@ -0,0 +1,27 @@
+import "./RainEffect.css";
+
+const RainEffect = () => {
+  type RainDrop = {
+    left: string;
+    animationDelay: string;
+  };
+
+  const createRaindrops = (numDrops: number): JSX.Element[] => {
+    const drops: JSX.Element[] = [];
+    for (let i = 0; i < numDrops; i++) {
+      const style: RainDrop = {
+        left: `${Math.floor(Math.random() * window.innerWidth)}px`,
+        animationDelay: `${Math.random() * 4}s`,
+      };
+      const item = <div key={i} className="rain" style={style}></div>;
+      drops.push(item);
+    }
+    return drops;
+  };
+
+  const raindrops = createRaindrops(50);
+  console.log(raindrops);
+  return <div className="rain-container">{raindrops}</div>;
+};
+
+export default RainEffect;
