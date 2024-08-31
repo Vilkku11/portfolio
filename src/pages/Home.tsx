96d3ae32fb96b7ccbd2b65fad043d013c76b1793
William Reima <william.reima@gmail.com>
2024-08-31 23:19:55 +0300

    gradient background to init page, dep update, removed workflow for now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1 className="home-header">is this home??</h1>
-      <a className="home-link" href="/w">
-        Click me
-      </a>
+    <div className="background-home">
+      <div className="container">
+        <h1 className="home-header">is this home??</h1>
+        <a className="home-link" href="/w">
+          Click me!!!
+        </a>
+      </div>
     </div>
   );
 };
