0331894538693a7be68e2e5310d55d3aa59fe978
William Reima <william.reima@gmail.com>
2024-09-21 00:30:02 +0300

    footer item design start, attributions page init, dep update

---
+++ src/pages/attributions/Attributions.tsx
@@ -0,0 +1,11 @@
+import "./Attributions.css";
+
+const Attributions = () => {
+  return (
+    <div className="attributions-items">
+      <h1 className="attributions-header">Credits & Attributions</h1>
+    </div>
+  );
+};
+
+export default Attributions;
