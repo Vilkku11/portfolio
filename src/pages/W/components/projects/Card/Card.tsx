fd553bd5c63c8f39e5e1907658b02d58bf36199a
William Reima <william.reima@gmail.com>
2024-12-28 23:13:29 +0200

    initial card styling and content, set sectionobserver behind everything else

--- src/pages/W/components/projects/Card/Card.tsx
+++ src/pages/W/components/projects/Card/Card.tsx
@@ -1,4 +1,5 @@
-import { motion, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
 import { Project } from "../../../Data";
 import ProjectLink from "../../projectLink/ProjectLink";
 import "./Card.css";
@@ -16,22 +17,45 @@ const Card = ({
   targetScale: number;
   progress: any;
 }) => {
-  const scale: any = useTransform(progress, range, [1, targetScale]);
+  const container = useRef<null | HTMLDivElement>(null);
+
+  const { scrollYProgress }: { scrollYProgress: any } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+  const cardScale: any = useTransform(progress, range, [1, targetScale]);
+  const imageScale: any = useTransform(scrollYProgress, [0, 1], [1.2, 1]);
 
   return (
-    <div className="project-card-container">
+    <div ref={container} className="project-card-container">
       <motion.div
         style={{
-          scale,
+          scale: cardScale,
+          //backgroundColor: project.color,
           backgroundColor: project.color,
+          backdropFilter: "blur(50px)",
           top: `calc(-10% + ${index * 20}px)`,
         }}
         className="project-card-style"
       >
-        <h1>{project.title}</h1>
-        <ProjectLink url={project.link} />
-        <p>{project.description}</p>
-        <p>{project.link}</p>
+        <h1 className="project-header">{project.title}</h1>
+        <div className="content-divider">
+          <div className="text-container">
+            <ProjectLink url={project.link} />
+            <p>{project.description}</p>
+            <p>{project.link}</p>
+          </div>
+          <div className="image-container">
+            <motion.img
+              style={{
+                scale: imageScale,
+              }}
+              className="image"
+              src={project.image}
+              alt=""
+            />
+          </div>
+        </div>
       </motion.div>
     </div>
   );
