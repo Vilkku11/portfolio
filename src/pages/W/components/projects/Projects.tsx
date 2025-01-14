78c6e4c50e35613458145171d2988ca3657cec19
William Reima <william.reima@gmail.com>
2025-01-14 18:18:10 +0200

    Projects header fix, sectionObserver props fix

--- src/pages/W/components/projects/Projects.tsx
+++ src/pages/W/components/projects/Projects.tsx
@@ -16,21 +16,24 @@ const Projects = () => {
   const itemRange: number = 1 / projects.length;
 
   return (
-    <div ref={container} className="projects-container" id="projects">
-      {projects.map((project: Project, index: number) => {
-        const targetScale: number = 1 - (projects.length - index) * 0.05;
-        return (
-          <Card
-            key={index}
-            index={index}
-            project={project}
-            range={[index * itemRange, 1]}
-            targetScale={targetScale}
-            progress={scrollYProgress}
-          />
-        );
-      })}
-      <SectionObserver sectionName="Projects" />
+    <div>
+      <h1 className="projects-header">Projects</h1>
+      <div ref={container} className="projects-container" id="projects">
+        {projects.map((project: Project, index: number) => {
+          const targetScale: number = 1 - (projects.length - index) * 0.05;
+          return (
+            <Card
+              key={index}
+              index={index}
+              project={project}
+              range={[index * itemRange, 1]}
+              targetScale={targetScale}
+              progress={scrollYProgress}
+            />
+          );
+        })}
+        <SectionObserver sectionName="Projects" />
+      </div>
     </div>
   );
 };
