2e49b7a429ec708fa53af9d63096a50e922b8a57
William Reima <william.reima@gmail.com>
2026-06-03 20:36:38 +0300

    refactor: remove obsolete Attributions page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
 
 import Portfolio from "./pages/W/Portfolio";
-import Attributions from "./pages/attributions/Attributions";
 
 import "./App.css";
 
@@ -9,7 +8,6 @@ function App() {
   const routes = (
     <Routes>
       <Route path="/" element={<Portfolio />} />
-      <Route path="/attributions" element={<Attributions />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
